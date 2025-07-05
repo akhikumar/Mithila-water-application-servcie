ab945c70f174efe79c64ddea020ae0d3d25e60af
chand <chand@CHANDAN>
2025-07-05 17:45:04 +0530

    initial commit for Mithila-water application..

---
+++ MithilaWaterApplicationService/src/main/java/com/mithilaWater/application/MithilaWaterApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mithilaWater.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MithilaWaterApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MithilaWaterApplicationServiceApplication.class, args);
+	}
+
+}
